c77f8f034df1d8e2a4c4c6302e2b6895cbe02ae0
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2026-05-10 19:23:02 +0530

    [feat]: add delete command: --task-cli=delete -delete id

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"slices"
 )
 
 type Task struct {
@@ -44,11 +45,15 @@ func main() {
 	task_cli := flag.String("task-cli", "add", "a task for the user can add/update/delete task")
 	task := flag.String("add", "todo", "a task for the user can add to a task")
 	task_id := flag.Int("id", 0, "an identifer for the task") // TODO: remove it for add
-	// task_description := flag.String("")
+	// command: delete
+	task_delete := flag.Int("delete", 0, "delete a task with an id")
+	//command: mark-in-progress
+	task_mark_in_progress := flag.Int("mark-in-progress", 0, "mark a task as in-progress")
 	flag.Parse()
 	fmt.Printf("given command is %s \n", *task_cli)
 	fmt.Printf("given task is %s \n", *task)
 	fmt.Printf("id is %d \n", *task_id)
+	fmt.Printf("delete is %d \n", *task_delete)
 
 	newTaskID := len(allTasks) + 1
 
@@ -58,11 +63,34 @@ func main() {
 
 	allTasks = append(allTasks, newTask)
 
+	if *task_delete != 0 {
+		updatedTasks := deleteTaskByID(task_delete, allTasks)
+		fmt.Printf("%+v\n", updatedTasks)
+		data, _ := json.MarshalIndent(updatedTasks, "", "  ") // convert []byte
+		os.WriteFile(filename, data, 0644)
+		os.Exit(1)
+	}
+
 	data, _ := json.MarshalIndent(allTasks, "", "  ") // convert []byte
 
 	os.WriteFile(filename, data, 0644)
+
+	fmt.Printf("Task added successfully (ID: %d)", newTaskID)
 	defer file.Close()
+}
+
+func deleteTaskByID(id *int, t []Task) []Task {
+	t = slices.DeleteFunc(t, func(task Task) bool {
+		return *task.ID == *id
+	})
+
+	fmt.Printf("deleted %d", *id)
+	return t
 }
 
 // 1. get type of task - add, update, delete, mark-in-progress, mark-done, list
 // go run main.go --task-cli=add -add "sell mango" -id 3
+
+// TODO: reove id in flag
+// TODO: handle same task already exists
+// TODO: prints the address here fmt.Printf("%+v\n", updatedTasks)
